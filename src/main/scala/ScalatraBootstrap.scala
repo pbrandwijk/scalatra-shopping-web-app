74faf294b5de41e30e147e8a3f6a17d798807f60
Pieter Brandwijk <pieter@pbrandwijk.com>
2018-08-05 21:48:52 +0200

    #9 Add empty UserServlet and add it to ScalatraBootstrap

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,10 +1,10 @@
-import com.pbrandwijk.app._
-import com.pbrandwijk.app.servlets.ProductServlet
+import com.pbrandwijk.app.servlets.{ProductServlet, UserServlet}
 import org.scalatra._
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext) {
-    context.mount(new ProductServlet, "/*")
+    context.mount(new ProductServlet, "/products/*")
+    context.mount(new UserServlet, "/users/*")
   }
 }
---
+++ src/main/scala/com/pbrandwijk/app/servlets/UserServlet.scala
@@ -0,0 +1,11 @@
+package com.pbrandwijk.app.servlets
+
+import org.scalatra._
+import org.slf4j.LoggerFactory
+
+class UserServlet extends ScalatraServlet {
+
+  // create logger for class
+  val logger =  LoggerFactory.getLogger(getClass)
+
+}
