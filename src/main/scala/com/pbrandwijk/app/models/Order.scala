88f8a309231cd3a80746fa9f633552a4814993c6
Pieter Brandwijk <pieter@pbrandwijk.com>
2018-08-06 20:47:37 +0200

    #14 Change member field "total" to "totalPrice" in class order

--- src/main/scala/com/pbrandwijk/app/models/Order.scala
+++ src/main/scala/com/pbrandwijk/app/models/Order.scala
@@ -7,9 +7,9 @@ import scala.collection.mutable.HashMap
   *
   * @param email The uniquely identifying email address of the user
   * @param address The shipping address of thu user
-  * @param total The total price of the order
+  * @param totalPrice The total price of the order
   * @param items The collection of product codes and their quantities in the order
   */
-class Order (val email: String, val address: String, val total: Double, var items: HashMap[String,Int]) {
-  override def toString = email + ", " + address + ", " + total + ", " + items
+class Order (val email: String, val address: String, val totalPrice: Double, var items: HashMap[String,Int]) {
+  override def toString = email + ", " + address + ", " + totalPrice + ", " + items
 }
